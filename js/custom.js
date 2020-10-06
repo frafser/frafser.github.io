4e8d82298f48351e75fe17d1302a8b7313e54a5d
frafser <68514839+frafser@users.noreply.github.com>
2020-10-07 04:30:22 +0600

    07-oct-2020

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+var typed = new Typed('#typed', {
+    strings: ['Web Designer.','Freelancer.','Web Developer.'],
+    smartBackspace: true,
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true,
+});
+/*End typed*/ 
+$("#banner").jParticle({
+    background:'transparent',
+    color: 'white',
+    particlesNumber: 30,
+
+});
